1650d3dee54ae55c19b06cec730e692d67e6d300
noClaps <04plugs-bios@icloud.com>
2024-10-25 01:20:11 +0100

    feat: Add install link on landing page

--- src/register.ts
+++ src/register.ts
@@ -4,32 +4,32 @@ const token = Bun.env.DISCORD_TOKEN;
 const applicationId = Bun.env.DISCORD_APPLICATION_ID;
 
 if (!token) {
-	throw new Error("Discord token not found");
+  throw new Error("Discord token not found");
 }
 if (!applicationId) {
-	throw new Error("Application ID not found");
+  throw new Error("Application ID not found");
 }
 
 async function registerCommands(url: string) {
-	const response = await fetch(url, {
-		headers: {
-			"content-type": "application/json",
-			authorization: `Bot ${token}`,
-		},
-		method: "PUT",
-		body: JSON.stringify([CAT_COMMAND]),
-	});
+  const response = await fetch(url, {
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bot ${token}`,
+    },
+    method: "PUT",
+    body: JSON.stringify([CAT_COMMAND]),
+  });
 
-	if (response.ok) {
-		console.log("Registered all commands!");
-	} else {
-		console.error("Error registering commands");
-		console.error(await response.text());
-	}
+  if (response.ok) {
+    console.log("Registered all commands!");
+  } else {
+    console.error("Error registering commands");
+    console.error(await response.text());
+  }
 
-	return response;
+  return response;
 }
 
 await registerCommands(
-	`https://discord.com/api/v10/applications/${applicationId}/commands`,
+  `https://discord.com/api/v10/applications/${applicationId}/commands`,
 );
