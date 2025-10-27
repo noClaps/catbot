d61aa319adeeb8c89936fa721a77799bfacdc834
noClaps <git@zerolimits.dev>
2025-10-27 10:25:21 +0000

    Remove Hono dependency

--- src/index.ts
+++ src/index.ts
@@ -3,43 +3,45 @@ import {
   InteractionType,
   verifyKey,
 } from "discord-interactions";
-import { Hono, HonoRequest } from "hono";
 import { CAT_COMMAND, catCommand } from "./commands";
 
-const router = new Hono();
-
-router.get("/", (c) => {
-  return c.html(
-    `<a href="https://discord.com/oauth2/authorize?client_id=1271901024910839959">Install CatBot</a>`,
-  );
-});
-
-router.post("/", async (c) => {
-  const { req, env } = c;
-
-  const { isValid, interaction } = await server.verifyDiscordRequest(req, env);
-  if (!isValid || !interaction) {
-    return new Response("Bad request signature", { status: 401 });
-  }
-
-  if (interaction.type === InteractionType.PING) {
-    return Response.json({ type: InteractionResponseType.PONG });
-  }
-
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    if (
-      interaction.data.name.toLowerCase() !== CAT_COMMAND.name.toLowerCase()
-    ) {
-      return Response.json({ error: "Unknown type" }, { status: 400 });
+async function fetch(req: Request): Promise<Response> {
+  switch (req.method) {
+    case "GET": {
+      return new Response(
+        `<a href="https://discord.com/oauth2/authorize?client_id=1271901024910839959">Install CatBot</a>`,
+        { headers: { "Content-Type": "text/html;charset=UTF-8" } },
+      );
     }
+    case "POST": {
+      const env = process.env;
+      const { isValid, interaction } = await verifyDiscordRequest(req, env);
+      if (!isValid || !interaction) {
+        return new Response("Bad request signature", { status: 401 });
+      }
 
-    return await catCommand(interaction);
+      if (interaction.type === InteractionType.PING) {
+        return Response.json({ type: InteractionResponseType.PONG });
+      }
+
+      if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+        if (
+          interaction.data.name.toLowerCase() !== CAT_COMMAND.name.toLowerCase()
+        ) {
+          return Response.json({ error: "Unknown type" }, { status: 400 });
+        }
+
+        return await catCommand(interaction);
+      }
+    }
+    default:
+      return Response.error();
   }
-});
+}
 
-async function verifyDiscordRequest(request: HonoRequest, env: any) {
-  const signature = request.header("x-signature-ed25519");
-  const timestamp = request.header("x-signature-timestamp");
+async function verifyDiscordRequest(request: Request, env: any) {
+  const signature = request.headers.get("x-signature-ed25519");
+  const timestamp = request.headers.get("x-signature-timestamp");
   const body = await request.text();
   const isValidRequest =
     signature &&
@@ -55,9 +57,6 @@ async function verifyDiscordRequest(request: HonoRequest, env: any) {
   };
 }
 
-const server = {
-  verifyDiscordRequest,
-  fetch: router.fetch,
+export default {
+  fetch,
 };
-
-export default server;
