73e0bb8d39cd9f149bf9c323e0bb7563d59bbcd8
noClaps <git@zerolimits.dev>
2025-10-27 10:37:15 +0000

    Remove HTML page

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,6 @@ type Env = {
   DISCORD_PUBLIC_KEY: string;
 };
 
-async function fetch(req: Request, env: Env): Promise<Response> {
-  switch (req.method) {
-    case "GET": {
-      return new Response(
-        `<a href="https://discord.com/oauth2/authorize?client_id=1271901024910839959">Install CatBot</a>`,
-        { headers: { "Content-Type": "text/html;charset=UTF-8" } },
-      );
-    }
-    case "POST": {
-      const { isValid, interaction } = await verifyDiscordRequest(req, env);
-      if (!isValid || !interaction) {
-        return new Response("Bad request signature", { status: 401 });
-      }
-
-      if (interaction.type === InteractionType.PING) {
-        return Response.json({ type: InteractionResponseType.PONG });
-      }
-
-      if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-        if (
-          interaction.data.name.toLowerCase() !== CAT_COMMAND.name.toLowerCase()
-        ) {
-          return Response.json({ error: "Unknown type" }, { status: 400 });
-        }
-
-        return catCommand(interaction);
-      }
-    }
-    default:
-      return Response.error();
-  }
-}
-
 async function verifyDiscordRequest(request: Request, env: Env) {
   const signature = request.headers.get("x-signature-ed25519");
   const timestamp = request.headers.get("x-signature-timestamp");
@@ -61,5 +28,26 @@ async function verifyDiscordRequest(request: Request, env: Env) {
 }
 
 export default {
-  fetch,
+  async fetch(req: Request, env: Env) {
+    const { isValid, interaction } = await verifyDiscordRequest(req, env);
+    if (!isValid || !interaction) {
+      return new Response("Bad request signature", { status: 401 });
+    }
+
+    if (interaction.type === InteractionType.PING) {
+      return Response.json({ type: InteractionResponseType.PONG });
+    }
+
+    if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+      if (
+        interaction.data.name.toLowerCase() !== CAT_COMMAND.name.toLowerCase()
+      ) {
+        return Response.json({ error: "Unknown type" }, { status: 400 });
+      }
+
+      return catCommand(interaction);
+    }
+
+    return new Response("Not found", { status: 404 });
+  },
 };
